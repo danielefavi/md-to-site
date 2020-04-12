874ad1e4ff6876f31cb4d0275821482b623bf4a6
piripoppi <dan.favi@gmail.com>
2020-04-12 06:57:07 +0200

    Example website

---
+++ js/searchIndex.js
@@ -0,0 +1,6 @@
+
+var searchIndex = [];
+searchIndex[0]={f:'Markdown-Cheatsheet.html',t:'Markdown Cheatsheet',c:['Headers','H1','H2','H3','H4','H5','H6','Alt-H1','Alt-H2','Emphasis','Lists','Links','Images','Code and Syntax Highlighting','Tables','Blockquotes','Inline HTML','Horizontal Rule','Line Breaks','YouTube Videos',]};
+searchIndex[1]={f:'README.html',t:'Readme',c:['Markdown to Site','Installation','How to convert markdown files to a website','List of parameters',]};
+searchIndex[2]={f:'Submenu_Ethereum VUE.JS DAPP.html',t:'Ethereum Vue.js Dapp',c:['Ethereum and VueJS DApp','Brief description','Prerequisite','Quick Installation',]};
+searchIndex[3]={f:'Submenu_MVC Framework.html',t:'Mvc Framework',c:['php-mvc','When you should use it?','Quickstart',]};
