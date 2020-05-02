e7e047d03c18f6d608f2b0f2e8ae96f839668591
DESKTOP-9AP0MEV\Daniele <dan.favi@gmail.com>
2020-05-02 11:37:09 +0200

    Example website update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # md-to-site-example.github.io
-Example of website create with the NPM package md-to-site
+Example of website create with the **md-to-site** NPM package.
+
+For more info plese check: 
+
+- **NPM package**: [https://www.npmjs.com/package/md-to-site](https://www.npmjs.com/package/md-to-site)# List of parameters
+- **Github Repo**: [https://github.com/danielefavi/md-to-site/](https://github.com/danielefavi/md-to-site/)
--- js/searchIndex.js
+++ js/searchIndex.js
@@ -1,6 +1,8 @@
 
 var searchIndex = [];
-searchIndex[0]={f:'Markdown-Cheatsheet.html',t:'Markdown Cheatsheet',c:['Headers','H1','H2','H3','H4','H5','H6','Alt-H1','Alt-H2','Emphasis','Lists','Links','Images','Code and Syntax Highlighting','Tables','Blockquotes','Inline HTML','Horizontal Rule','Line Breaks','YouTube Videos',]};
-searchIndex[1]={f:'README.html',t:'Readme',c:['Markdown to Site','Installation','How to convert markdown files to a website','List of parameters',]};
-searchIndex[2]={f:'Submenu_Ethereum VUE.JS DAPP.html',t:'Ethereum Vue.js Dapp',c:['Ethereum and VueJS DApp','Brief description','Prerequisite','Quick Installation',]};
-searchIndex[3]={f:'Submenu_MVC Framework.html',t:'Mvc Framework',c:['php-mvc','When you should use it?','Quickstart',]};
+searchIndex[0]={f:'ethereum-vuejs-dapp.html',t:'Ethereum Vuejs Dapp',c:['Ethereum and VueJS DApp','Brief description','Prerequisite','Quick Installation',]};
+searchIndex[1]={f:'machine-learning-php-class.html',t:'Machine Learning Php Class',c:['Brainy: a simple PHP class for machine learning',]};
+searchIndex[2]={f:'Markdown-Cheatsheet.html',t:'Markdown Cheatsheet',c:['Markdown Cheatsheet','Headers','H1','H2','H3','H4','H5','H6','Alt-H1','Alt-H2','Emphasis','Lists','Links','Images','Code and Syntax Highlighting','Tables','Blockquotes','Inline HTML','Horizontal Rule','Line Breaks','YouTube Videos',]};
+searchIndex[3]={f:'README.html',t:'Readme',c:['Markdown to Site','Demo','Installation','How to convert markdown files to a website','Notes','List of Parameters','Links',]};
+searchIndex[4]={f:'Others_free-the-bean.html',t:'Free The Bean',c:['Free The Bean!',]};
+searchIndex[5]={f:'Others_wp-social-importer.html',t:'Wp Social Importer',c:['WP Social Importer - Wordpress Plugin',]};
